3d7f8249947b41f9ab5b7d326921b0c71cef9186
Yiska08 <graciellayiska08@gmail.com>
2025-07-07 12:16:15 -0700

    refactor: fixed animate function

--- src/PlatformerGame.js
+++ src/PlatformerGame.js
@@ -1,47 +1,46 @@
 import React, { useRef, useEffect, useState } from "react";
 import Player from "./Player";
 
-
-
 const PlatformerGame = ({width, height, tilesize}) => {
-    
-    
     const canvasRef= useRef();
     const [player, setPlayer] = useState(new Player());
 
     useEffect(() =>{
-        console.log('Draw to canvas');
         const ctx = canvasRef.current.getContext('2d');
+        console.log('Draw to canvas');
         ctx.clearRect(0,0,width*tilesize,height*tilesize);
-        animate(ctx);
 
+        function loop() {
+            animate(ctx); // correctly pass the context
+            requestAnimationFrame(loop); // loop properly
+        }
+
+        loop()
     });
 
     function animate(context)
     {
-        const ctx = canvasRef.current.getContext('2d');
-        window.requestAnimationFrame(animate);
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0,0,1120,800);
-        player.update(ctx);
-
+        context.fillStyle = 'white';
+        context.fillRect(0,0,1120,800);
+        player.update(context);
     }
+
     return (
     <div
         style={{
-        display: 'flex',
-        justifyContent: 'center', //Centered canvas 
-        alignItems: 'center', 
-        height: '100vh', // full screen height
-        backgroundColor: '#f0f0f0',
+            display: 'flex',
+            justifyContent: 'center', //Centered canvas 
+            alignItems: 'center', 
+            height: '100vh', // full screen height
+            backgroundColor: '#f0f0f0',
       }}
     >
+
         <canvas 
-        ref={canvasRef}
-        width={width * tilesize}
-        height={height * tilesize}
-        style={{border: '1px solid black'}}
-        
+            ref={canvasRef}
+            width={width * tilesize}
+            height={height * tilesize}
+            style={{border: '1px solid black'}}
         ></canvas>
 
     </div>
